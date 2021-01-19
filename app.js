468fc54b22c27f0661ad5b2f373d30821fa57685
Sneha Pujari <58720128+sneha-pujari@users.noreply.github.com>
2021-01-19 11:25:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,71 +15,33 @@ function hexToRgb(hex) {
     const resultDiv = document.getElementById('result');
     
     if (inputValue.includes('#') && inputValue.length <= 7) {
-      resultDiv.innerHTML = `<h3>rgb(${hexToRgb(inputValue)})</h3>`;
-      document.body.style.color = lightOrDark(hexToRgb(inputValue));
+      resultDiv.innerHTML = `<h1>rgb(${hexToRgb(inputValue)})</h1>`;
+     // document.body.style.color = lightOrDark(hexToRgb(inputValue));
 
-    //document.body.style.backgroundColor = rgb(hexToRgb(inputValue)); 
+   
     console.log(hexToRgb(inputValue))
-    } else {
+    } 
+    
+    else {
       inputValue = inputValue.replace(/(rgb)|\(|\)/g, '')
                              .split(',')
                              .map(val => Number(val));
       
       resultDiv.innerHTML = `<h3>${rgbToHex(inputValue)}</h3>`;
-    //   document.body.style.backgroundColor = {rgbToHex(inputValue)};
     console.log(rgbToHex(inputValue)) 
-    document.body.style.color = lightOrDark(rgbToHex(inputValue));
-    // document.body.style.backgroundColor = result.value;
+    //document.body.style.color = lightOrDark(rgbToHex(inputValue));
+ 
     }
-   var res = document.querySelector("#result");
+   
+   //body background color
+    var res = document.querySelector("#result");
    console.log("result"+res.textContent);
    document.body.style.backgroundColor = res.textContent;
+
 //    if(res.textContent === "")
   }
  
-  function lightOrDark(color) {
-
-    // Variables for red, green, blue values
-    var r, g, b, hsp;
-    
-    // Check the format of the color, HEX or RGB?
-    if (color.match(/^rgb/)) {
-
-        // If RGB --> store the red, green, blue values in separate variables
-        color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
-        
-        r = color[1];
-        g = color[2];
-        b = color[3];
-    } 
-    else {
-        
-        // If hex --> Convert it to RGB: http://gist.github.com/983661
-        color = +("0x" + color.slice(1).replace( 
-        color.length < 5 && /./g, '$&$&'));
-
-        r = color >> 16;
-        g = color >> 8 & 255;
-        b = color & 255;
-    }
-    
-    // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
-    hsp = Math.sqrt(
-    0.299 * (r * r) +
-    0.587 * (g * g) +
-    0.114 * (b * b)
-    );
-
-    // Using the HSP value, determine whether the color is light or dark
-    if (hsp>127.5) {
-       console.log('light');
-        return '#000000';
-    } 
-    else {
-        console.log('dark');
-        return '#ffffff';
-    }
-}
+  
   document.getElementById('convert-form').onsubmit = function(event) {
     event.preventDefault();
     validateInput(document.getElementById('convert-input').value);
